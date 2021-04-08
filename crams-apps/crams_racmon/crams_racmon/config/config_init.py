aacc06d6121515e9155afdfdd07f56f2104a7a4b
Rafi M Feroze <mohamed.feroze@monash.edu>
2021-04-08 15:58:24 +1000

    update the sample erb system CRAMS-ERB-SYS to use Application Updated status for non-quota changes

---
+++ crams-apps/crams_racmon/crams_racmon/config/config_init.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+"""
+
+"""
+
+from crams_allocation.config.allocation_config import EXTEND_ON_QUOTA_CHANGE
+
+# TODO: move the sample ERB system sqls for CRAMS-ERB and CRAMS-ERB-SYS to this module (from merc-common)
+EXTEND_ON_QUOTA_CHANGE.append('CRAMS-ERB-SYS'.lower())

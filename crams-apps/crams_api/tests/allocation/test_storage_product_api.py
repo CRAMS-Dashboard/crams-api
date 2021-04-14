f3762d31641283ea21acc8376d6858908f9e00cb
melvin-luong <melvin.luong@monash.edu>
2021-04-14 16:57:00 +1000

    Updated storage product api test case to use test util data.

--- crams-apps/crams_api/tests/allocation/test_storage_product_api.py
+++ crams-apps/crams_api/tests/allocation/test_storage_product_api.py
@@ -1,17 +1,20 @@
 from rest_framework.reverse import reverse
 
-from crams.test_utils import CommonBaseTestCase
+from crams_allocation.test_utils import UnitTestCase
 
 
-class TestStorageProductApi(CommonBaseTestCase):
+class TestStorageProductApi(UnitTestCase):
     def setUp(self):
+        super().setUp()
+        self.generate_allocation_test_data()
         self.client = self.auth_client()
+        
 
     def test_storage_products_by_e_r_body_name(self):
-        storage_product_api_url = reverse('storage_products', kwargs={"fb_name": 'NeCTAR'})
+        storage_product_api_url = reverse('storage_products', kwargs={"fb_name": self.funding_body.name.lower()})
         assert storage_product_api_url != None
         storage_products_response = self.client.get(storage_product_api_url)
         assert storage_products_response.status_code == 200
         json_data = storage_products_response.data
         assert len(json_data) > 0
-        assert json_data[0]['name'] == 'Volume (Intersect)'
+        assert json_data[0]['name'] == self.storage_prod.name

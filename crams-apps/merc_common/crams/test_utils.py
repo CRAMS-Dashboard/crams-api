63b184de2913f51bc6e722c7d8331a1a7a086c02
melvin-luong <melvin.luong@monash.edu>
2021-04-16 16:31:45 +1000

    Updated the auth client to use the ContactErbRoleSerializer to generate the crams token with the user role permissions.

--- crams-apps/merc_common/crams/test_utils.py
+++ crams-apps/merc_common/crams/test_utils.py
@@ -3,6 +3,7 @@ from hypothesis.extra.django import TestCase
 from mixer.backend.django import mixer
 from rest_framework.test import APIClient
 
+from crams_contact.serializers.contact_erb_roles import ContactErbRoleSerializer
 from crams.models import CramsToken
 from crams.utils import crams_utils
 
@@ -22,7 +23,7 @@ class CommonBaseTestCase(TestCase):
         # force user to login
         client.force_authenticate(user=user)
         if not token:
-            token, created = CramsToken.objects.get_or_create(user=user)
+            token = ContactErbRoleSerializer.setup_crams_token_and_roles(user)
 
         client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
         return client

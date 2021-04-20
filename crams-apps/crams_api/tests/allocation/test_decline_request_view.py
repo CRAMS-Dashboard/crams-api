855cc0a138f48657ec3b3a2ac9e5bb51a0e60f94
melvin-luong <melvin.luong@monash.edu>
2021-04-20 14:59:55 +1000

    Added allocation extension decline test case.

--- crams-apps/crams_api/tests/allocation/test_decline_request_view.py
+++ crams-apps/crams_api/tests/allocation/test_decline_request_view.py
@@ -18,8 +18,8 @@ class DeclineRequestViewTest(UnitTestCase):
         self.generate_allocation_test_data()
         self.client = self.auth_client(user=self.approver_user)
 
-    def test_decline_request_view(self):
-        req = self.prj_submit.requests.all().first()
+    def _test_decline_request_view(self, project):
+        req = project.requests.all().first()
         url = reverse('decline-request-detail', args=[req.id])
         url = '/decline_request/{}/'.format(req.id)
         # set up payload
@@ -35,7 +35,21 @@ class DeclineRequestViewTest(UnitTestCase):
         sub_req = Request.objects.filter(current_request=decl_req).first()
         # check submit request parent id matches new declined request id
         assert sub_req
-        # check request has been declined
-        assert decl_req.request_status.code == db.REQUEST_STATUS_DECLINED
+        
         # check declined approval notes
         assert decl_req.approval_notes == payload['approval_notes']
+
+        # return status code for check request has been declined
+        return decl_req.request_status.code
+
+    def test_decline_request_view_submission(self):
+        # test a new submitted project request for decline
+        code = self._test_decline_request_view(self.prj_submit)
+        assert code == db.REQUEST_STATUS_DECLINED
+
+
+    def test_decline_request_view_extension(self):
+        # test an existing provisioned project request that 
+        # has its quota extended for decline
+        code = self._test_decline_request_view(self.prj_alloc_ext)
+        assert code == db.REQUEST_STATUS_UPDATE_OR_EXTEND_DECLINED

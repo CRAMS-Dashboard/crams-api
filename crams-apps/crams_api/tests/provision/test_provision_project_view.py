f5b9dcf11dc29e12428927da823bcd3826232657
melvin-luong <melvin.luong@monash.edu>
2021-04-20 10:07:21 +1000

    Updated provision tests to use new provision api. Updated the provision test util json payload.

--- crams-apps/crams_api/tests/provision/test_provision_project_view.py
+++ crams-apps/crams_api/tests/provision/test_provision_project_view.py
@@ -10,6 +10,7 @@ from crams.constants import db
 from crams.utils.crams_utils import get_random_string
 from crams_allocation.test_utils import UnitTestCase
 from crams_allocation.models import Request
+from crams_allocation.product_allocation.models import StorageRequest
 from crams_provision.test_utils import UnitTestCase as ProvTestCase
 
 class ProvisionProjectViewTest(UnitTestCase, ProvTestCase):
@@ -45,15 +46,40 @@ class ProvisionProjectViewTest(UnitTestCase, ProvTestCase):
         assert response.data['requests'][0]['storage_requests'][0]['approved_quota_change'] == sp_req.approved_quota_change
         assert response.data['requests'][0]['storage_requests'][0]['approved_quota_total'] == sp_req.approved_quota_total
 
-    def test_provision_project_view_update(self):
+    def test_provision_storage_request_view_update(self):
         # get request and project and set up the payload
-        payload = self.get_provision_json_payload(self.prj_approved)
-        url = "/provision_project/update/"
+        payload = list()
+        sp_req_list = self.prj_approved.requests.all().first().storage_requests.all()
+        for sp_req in sp_req_list:
+            payload.append(self.get_storage_request_provision(sp_req))
+        url = '/provision/storage_requests/update_provision_id_bulk/'
         response = self.client.post(url, data=json.dumps(payload), content_type="application/json")
-        assert response.status_code == 201
-        # check the approve request has been updated and get the new prov req
-        req_id = self.prj_approved.requests.all().first().id
-        appr_req = Request.objects.get(pk=req_id) # fetch the updated req
-        prov_req = Request.objects.get(pk=response.data['requests'][0]['id'])
-        assert appr_req.current_request == prov_req
-        assert prov_req.request_status.code == db.REQUEST_STATUS_PROVISIONED
+        assert response.status_code == 200
+        
+        # check new request status is 'P' Provisioned
+        storage_request = StorageRequest.objects.get(pk=response.data[0]['sp_provision']['id'])
+        assert storage_request.request.request_status.code == 'P'
+        for sp_req in sp_req_list:
+            sp_prov = next((x for x in response.data if x['sp_provision']['id'] == sp_req.id), None)
+            assert sp_prov
+            assert sp_prov['success'] == True
+            assert sp_prov['errors'] == None
+            assert sp_prov['sp_provision']['provision_id'] != None
+            assert sp_prov['sp_provision']['storage_product']['id'] == sp_req.storage_product.id
+
+    def test_provision_project_view_update_error(self):
+        # test provisioning with an existing provision id
+        # get existing provisioning id
+        storage_request = self.prj_prov.requests.all().first().storage_requests.all().first()
+        provision_id = storage_request.provision_id.provision_id
+        payload = list()
+        sp_req_list = self.prj_approved.requests.all().first().storage_requests.all()
+        for sp_req in sp_req_list:
+            payload.append(self.get_storage_request_provision(
+                sp_req, provision_id=provision_id))
+        url = '/provision/storage_requests/update_provision_id_bulk/'
+        response = self.client.post(url, data=json.dumps(payload), content_type="application/json")
+        assert response.status_code == 200
+        # check for errors
+        assert response.data[0]['success'] == False
+        assert response.data[0]['errors'] != None
--- crams-apps/crams_provision/crams_provision/test_utils.py
+++ crams-apps/crams_provision/crams_provision/test_utils.py
@@ -7,39 +7,33 @@ from crams.utils.crams_utils import get_random_string
 from crams.test_utils import CommonBaseTestCase
 
 class UnitTestCase(CommonBaseTestCase):
-    def get_request_dict(self, request):
-        return {
-            "id": request.id,
-            "compute_requests": [],
-            "storage_requests": [],
-            "sent_email": False
-        }
-    
-    def get_storage_request_dict(self, sp_req, provision_id=None):
+    def get_storage_request_provision(self, storage_request, provision_id=None):
         if not provision_id:
             provision_id = get_random_string(15)
 
         return {
-            "id": sp_req.id,
+            "id": storage_request.id,
             "provision_id": provision_id,
-            "success": True,
-            "message": "",
-            "storage_product": {
-                "id": sp_req.storage_product.id,
-                "storage_type": {
-                    "id": sp_req.storage_product.storage_type.id,
-                    "storage_type": sp_req.storage_product.storage_type.storage_type
-                },
-                "zone": self.get_zone_dict(sp_req.storage_product.zone),
-                "name": sp_req.storage_product.name,
-                "parent_storage_product": sp_req.storage_product.parent_storage_product
-            }
+            "message": get_random_string(50),
+            "storage_product": self._get_storage_product(storage_request.storage_product)
         }
     
-    def get_compute_request_dict(self):
+    def get_compute_request_provision(self):
         return None
 
-    def get_zone_dict(self, zone):
+    def _get_storage_product(self, storage_product):
+        return {
+                "id": storage_product.id,
+                "storage_type": {
+                    "id": storage_product.storage_type.id,
+                    "storage_type": storage_product.storage_type.storage_type
+                },
+                "zone": self._get_zone_dict(storage_product.zone),
+                "name": storage_product.name,
+                "parent_storage_product": storage_product.parent_storage_product
+            }
+
+    def _get_zone_dict(self, zone):
         if zone:
             return {
                 "id": zone.id,
@@ -48,24 +42,3 @@ class UnitTestCase(CommonBaseTestCase):
             }
         else:
             return None
-    
-    def get_provision_json_payload(self, project, provision_id=None):
-        request_list = list()
-        for req in project.requests.all():
-            req_dict = self.get_request_dict(req)
-            
-            sp_req_list = list()
-            for sp_req in req.storage_requests.all():
-                sp_req_dict = self.get_storage_request_dict(sp_req)
-                sp_req_list.append(sp_req_dict)
-
-            req_dict["storage_requests"] = sp_req_list
-            request_list.append(req_dict)
-        
-        return {
-            "id": project.id,
-            "success": True,
-            "message": "Provision message",
-            "project_ids": [],
-            "requests": request_list
-        }

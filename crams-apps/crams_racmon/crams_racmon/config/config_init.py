dc6968d1d11f42192e8519bf0ed9fe34a0a0f10e
Melvin Luong <>
2021-07-12 17:41:45 +1000

    Updated reply email into list.

--- crams-apps/crams_racmon/crams_racmon/config/config_init.py
+++ crams-apps/crams_racmon/crams_racmon/config/config_init.py
@@ -11,7 +11,7 @@ RDSM_ERB_LOWER = settings.CRAMS_DEMO_ERB.lower()
 ENABLE_EXT_SUPPORT_EMAIL = False
 
 # Support EMail
-RDSM_REPLY_TO_EMAIL = "rcmonsvradm1@gmail.com"
+RDSM_REPLY_TO_EMAIL = ["rcmonsvradm1@gmail.com"]
 racmon_support_email_dict = {
     'key': 'CRAMS',
     'email': 'rcmonsvradm1@gmail.com'}
